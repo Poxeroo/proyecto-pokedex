47b5efd2f81b19f972d5610bd2bce9a9a7062086
BranEstrella <102639643+BranEstrella@users.noreply.github.com>
2023-09-17 11:38:06 -0400

    solo falta redireccionar

--- src/app/componente/ComponentePokedex.js
+++ src/app/componente/ComponentePokedex.js
@@ -7,6 +7,19 @@ export default function mostrarPokedex() {
   const [selectedCategory, setSelectedCategory] = useState(""); // declaro las categorias de cada pokemon como un string
   const [searchTerm, setSearchTerm] = useState(""); // declaro el termino de busqueda como un string
 
+  //paginacion
+  const [pagina, setPagina] = useState(1); // aqui nos ubicaremos en la pagina que estemos
+  const [porPagina, setPorPagina] = useState(9); // aqui definimos la cantidad de pokemon que mostraremos por pagina
+
+  const cantidadPaginas = pokemonList.length / porPagina; // defino la cantidad de paginas que tendremos dividiendo el tamaño de la lista entre la cantidad de pokemon por pagina
+  
+  
+
+  
+
+
+
+
   useEffect(() => {
     //realizo una solicitud get a la api de pokemon para obtener los datos de los 151 pokemon
     axios
@@ -58,6 +71,12 @@ export default function mostrarPokedex() {
     pokemon.name.toLowerCase().includes(searchTerm.toLowerCase()) //aqui solo pido que ignoren si es minuscula o mayuscula
   );
 
+  const startIndex = (pagina - 1) * porPagina; // Calcula el índice inicial en función de la página actual
+  const endIndex = startIndex + porPagina; // Calcula el índice final
+
+  const paginatedPokemonList = searchFilteredPokemonList.slice(startIndex, endIndex); // Obtiene la lista de Pokémon para la página actual "averigua como funciona slice"
+  //slice lo que hace es recorrer un arreglo desde un punto de inicio hasta un punto final, por eso le pasamos el start que seria donde inicia esa pagina y el end donde termina
+
   return (
     <>
        {/* Agrega el selector de categorías */}
@@ -97,9 +116,9 @@ export default function mostrarPokedex() {
       
       {/* Mostrar la lista de Pokémon */}
       <div className="bg-slate-100 grid grid-cols-3 gap-4 p-8 shadow-inner" >
-        {searchFilteredPokemonList.map((pokemon, index) => (//mapeo toda la lista de pokemon siguiendo el filtro de busqueda pero esto no evita que funcionen todos los filtros
+        {paginatedPokemonList.map((pokemon, index) => (//mapeo toda la lista de pokemon siguiendo el filtro de busqueda por paginacion pero esto no evita que funcionen todos los filtros
           
-          <div className="bg-white rounded-xl shadow-sm shadow-slate-400 p-2" key={pokemon.id}>{/*Crea un div para mostrar los detalles de un Pokémon en particular. Se le da una clase CSS y una clave única (key) para identificación.*/}
+          <a className="bg-white rounded-xl shadow-sm shadow-slate-400 p-2" key={pokemon.id} href="#">{/*Crea un div para mostrar los detalles de un Pokémon en particular. Se le da una clase CSS y una clave única (key) para identificación.*/}
 
             <div className="flex justify-center">{/* Crea un div para mostrar una imagen del Pokémon. Se utiliza la URL de la imagen desde los detalles del Pokémon */}
               <img
@@ -117,15 +136,45 @@ export default function mostrarPokedex() {
 
             <div className="flex flex-row gap-x-3 justify-center py-2">
               {pokemon.categories.map((category, catIndex) => ( //(//mapeo el apartado type del contenido de pokemon y de ahi saco el nombre de su tipo y lo coloco como bg y como label del spam
-                <span key={catIndex} className={`text-black bg-${category} rounded-full text-lg font-semibold px-3`}>
+                <h3 key={catIndex} className={`text-black bg-${category} rounded-full text-lg font-semibold px-3`}>
                   {category}
-                </span>
+                </h3>
               ))}
               
             </div>
-          </div>
+          </a>
         ))}
       </div>
+
+      {/* Agregar la paginación */}
+    <div className="flex justify-center mt-4">
+      <button
+        onClick={() => setPagina((prev) => Math.max(prev - 1, 1))}
+        className="bg-slate-300 text-white px-4 py-2 rounded-lg"
+        disabled={pagina === 1}
+      >
+        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-arrow-big-left-filled" width="40" height="40" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
+          <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+          <path d="M9.586 4l-6.586 6.586a2 2 0 0 0 0 2.828l6.586 6.586a2 2 0 0 0 2.18 .434l.145 -.068a2 2 0 0 0 1.089 -1.78v-2.586h7a2 2 0 0 0 2 -2v-4l-.005 -.15a2 2 0 0 0 -1.995 -1.85l-7 -.001v-2.585a2 2 0 0 0 -3.414 -1.414z" stroke-width="0" fill="currentColor" />
+        </svg>
+
+      </button>
+      <span className="mx-4 text-black text-center flex items-center">
+        Página {Math.ceil(pagina)} de {Math.ceil(cantidadPaginas)} {/* math.ceil es para redondear al entero mayor mas cercano si es un numero entero no se redondea si no que se queda igual
+                                                                      se utiliza este porque al ser paginas se debe mostrar el mayor no el menor y en algunos casos se puede presentar esa situacion*/}
+      </span>
+      <button
+        onClick={() => setPagina((prev) => Math.min(prev + 1, cantidadPaginas))}
+        className="bg-slate-300 text-white px-4 py-2 rounded-lg"
+        disabled={pagina === Math.ceil(cantidadPaginas)}
+      >
+        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-arrow-big-right-filled" width="40" height="40" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
+          <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+          <path d="M12.089 3.634a2 2 0 0 0 -1.089 1.78l-.001 2.586h-6.999a2 2 0 0 0 -2 2v4l.005 .15a2 2 0 0 0 1.995 1.85l6.999 -.001l.001 2.587a2 2 0 0 0 3.414 1.414l6.586 -6.586a2 2 0 0 0 0 -2.828l-6.586 -6.586a2 2 0 0 0 -2.18 -.434l-.145 .068z" stroke-width="0" fill="currentColor" />
+        </svg>
+
+      </button>
+    </div>
     </>
   );
 }
